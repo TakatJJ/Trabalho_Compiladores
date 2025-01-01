559445ebf1115defe9a8e3ed2fe93a172709c983
Galeano Folle Domingues <galeanodomingues@gmail.com>
2025-01-01 16:38:46 -0300

    implementa and e or.

--- asm.cpp
+++ asm.cpp
@@ -4,7 +4,7 @@
 string ASM::generate_temp(string asm_code) {
     for (auto const& x : symbols_table) {
         if (x.second.text.find("__temp") != string::npos) {
-            asm_code += "\t.globl\t_" + x.first + "\n" + "\t.data\n\t.align 4\n_" + x.first + ":\n\t.long\t0\n";
+            asm_code += "\t.globl\t_" + x.first + "\n" + "\t.data\n\t.align\t4\n_" + x.first + ":\n\t.long\t0\n";
         }
     }
     return asm_code;
@@ -13,13 +13,13 @@ string ASM::generate_temp(string asm_code) {
 string ASM::generate_literals(string asm_code) { //literais char string e inteiros começam com _@
     for (auto const& x : symbols_table) {
         if (x.second.type == SYMBOL_LIT_STRING) {
-            asm_code += "\t.globl\t_@" + x.first.substr(1,x.first.size()-2) + ":\n" + "\t.data\n\t.align 4\n_@"+ x.first.substr(1,x.first.size()-2) +":\n\t.ascii\t\""+ x.second.text.substr(1, x.first.size()-2) +"\\0\"\n";
+            asm_code += "\t.globl\t_@" + x.first.substr(1,x.first.size()-2) + ":\n" + "\t.data\n\t.align\t4\n_@"+ x.first.substr(1,x.first.size()-2) +":\n\t.ascii\t\""+ x.second.text.substr(1, x.first.size()-2) +"\\0\"\n";
         }
         else if (x.second.type == SYMBOL_LIT_CHAR) {
-            asm_code += "\t.globl\t_@" +x.first.substr(1,x.first.size()-2) + ":\n" + "\t.data\n\t.align 4\n_@"+ x.first.substr(1,x.first.size()-2) +":\n\t.long\t" + to_string(int(x.second.text.at(1))) + "\n";
+            asm_code += "\t.globl\t_@" +x.first.substr(1,x.first.size()-2) + ":\n" + "\t.data\n\t.align\t4\n_@"+ x.first.substr(1,x.first.size()-2) +":\n\t.long\t" + to_string(int(x.second.text.at(1))) + "\n";
         }
         else if (x.second.type == SYMBOL_LIT_INTEGER) {
-            asm_code += "\t.globl\t_@" + x.first + ":\n" + "\t.data\n\t.align 4\n_@"+ x.first +":\n\t.long\t"+ x.second.text.substr(1) +"\n";
+            asm_code += "\t.globl\t_@" + x.first + ":\n" + "\t.data\n\t.align\t4\n_@"+ x.first +":\n\t.long\t"+ x.second.text.substr(1) +"\n";
         }
     }
     return asm_code;
@@ -27,15 +27,15 @@ string ASM::generate_literals(string asm_code) { //literais char string e inteir
 
 string ASM::create_asm_variable(TAC* tac) {
     if (tac->res->data_type == DATA_TYPE_INT)
-        return "\tglobl\t_" + tac->res->text + "\n\t.data\n\t.align 4\n_" + tac->res->text + ":\n\t.long\t"+ tac->op1->text.substr(1) +"\n";
+        return "\tglobl\t_" + tac->res->text + "\n\t.data\n\t.align\t4\n_" + tac->res->text + ":\n\t.long\t"+ tac->op1->text.substr(1) +"\n";
     else if (tac->res->data_type == DATA_TYPE_CHAR)
-        return "\tglobl\t_" + tac->res->text + "\n\t.data\n\t.align 4\n_" + tac->res->text + ":\n\t.long\t" + to_string(int(tac->op1->text.at(1))) + "\n";
+        return "\tglobl\t_" + tac->res->text + "\n\t.data\n\t.align\t4\n_" + tac->res->text + ":\n\t.long\t" + to_string(int(tac->op1->text.at(1))) + "\n";
 }
 
 string ASM::create_asm_vector(TAC* tac, bool vec_has_init) {
     string code = "";
     if (!vec_has_init)
-        code = "\tglobl\t_" + tac->res->text + "\n\t.data\n\t.align 4\n_" + tac->res->text + ":\n";
+        code = "\tglobl\t_" + tac->res->text + "\n\t.data\n\t.align\t4\n_" + tac->res->text + ":\n";
     if (tac->op1->data_type == DATA_TYPE_INT)
         code += "\t.long\t"+tac->op1->text.substr(1) + "\n";
     else if (tac->op1->data_type == DATA_TYPE_CHAR)
@@ -66,15 +66,19 @@ string ASM::generate_variables(string asm_code, vector<TAC*> tacs){
             }
         }
     }
-    cout << asm_code << endl;
+    // cout << asm_code << endl;
     return asm_code;
 }
 
 string ASM::resolveSymbol(Symbol* symbol) {
     if (symbol->type == SYMBOL_LIT_STRING || symbol->type == SYMBOL_LIT_CHAR) {
+        if (symbol->text.at(0) == '@')
+            return symbol->text;
         return "@" + symbol->text.substr(1, symbol->text.size()-2);
     }
     else if (symbol->type == SYMBOL_LIT_INTEGER) {
+        if (symbol->text.at(0) == '@')
+            return symbol->text;
         return "@" + symbol->text;
     }
     else {
@@ -84,12 +88,13 @@ string ASM::resolveSymbol(Symbol* symbol) {
 
 void ASM::generate_ASM(vector<TAC*> tacs) {
     string asm_code = "";
+    int Lindex = 0;
 
     asm_code = generate_temp(asm_code);
     asm_code = generate_literals(asm_code);
     asm_code = generate_variables(asm_code, tacs);
 
-    asm_code += "\t.section .rdata,\"dr\"\nLC0:\n\t.ascii \"%d\\0\"\nLC1:\n\t.ascii \"%c\\0\"\n";
+    asm_code += "\t.section\t.rdata,\"dr\"\nLC0:\n\t.ascii\t\"%d\\0\"\nLC1:\n\t.ascii\t\"%c\\0\"\n";
     // ofstream output_file("output.asm");
     for (TAC* tac : tacs) {
         // Generate assembly code for each TAC
@@ -97,18 +102,70 @@ void ASM::generate_ASM(vector<TAC*> tacs) {
         switch(tac->type){
         case TAC_PRINT:{
             tac->res->text = resolveSymbol(tac->res);
-            
             if (tac->res->data_type == DATA_TYPE_CHAR)
-                asm_code+= "\tmovzbl	_"+tac->res->get_text()+", %eax\n\tmovsbl	%al, %eax\n\tmovl	%eax, (%esp)\n\tcall	_putchar\n";
+                asm_code+= "\tmovzbl\t_"+tac->res->get_text()+",\t%eax\n\tmovsbl\t%al,\t%eax\n\tmovl\t%eax,\t(%esp)\n\tcall\t_putchar\n";
             else if (tac->res->data_type == DATA_TYPE_STRING)
-                asm_code += "\tmovl	_"+ tac->res->get_text() +", (%esp)\n\tcall	_printf\n";
+                asm_code += "\tmovl\t_"+ tac->res->get_text() +",\t(%esp)\n\tcall\t_printf\n";
             else if (tac->res->data_type == DATA_TYPE_INT)
-                asm_code += "\tmovl	_"+tac->res->get_text()+", %eax\n\tmovl	%eax, 4(%esp)\n\tmovl	$LC0, (%esp)\n\tcall	_printf\n";
+                asm_code += "\tmovl\t_"+tac->res->get_text()+",\t%eax\n\tmovl\t%eax,\t4(%esp)\n\tmovl\t$LC0,\t(%esp)\n\tcall\t_printf\n";
             break;
         }
+        case TAC_VEC:
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+"+" +to_string(stoi(tac->op2->text.substr(1))*4)+", %eax\n\tmovl\t%eax,\t_"+ tac->res->get_text() + "\n";
+            break;
+        case TAC_MOVE:
+            tac->res->text = resolveSymbol(tac->res);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%eax\n\tmovl\t%eax,\t_"+tac->res->text + "\n";
+            break;
+        case TAC_ADD:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%edx\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\taddl\t%edx,\t%eax\n\tmovl\t%eax,\t_"+tac->res->get_text()+"\n";
+            break;
+        case TAC_SUB:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%edx\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\tsubl\t%edx,\t%eax\n\tmovl\t%eax,\t_"+tac->res->get_text()+"\n";
+            break;
+        case TAC_MUL:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%edx\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\timull\t%edx,\t%eax\n\tmovl\t%eax,\t_" + tac->res->get_text() + "\n";
+            break;
+        case TAC_DIV:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%eax\n\tcltd\n\tidivl\t_"+tac->op2->get_text()+"\n\tmovl\t%eax,\t_"+tac->res->get_text()+"\n";
+            break;
+        case TAC_EQUAL:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%edx\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\tcmpl\t%eax,\t%edx\n\tsete\t%al\n\tmovb\t%al,\t"+tac->res->get_text()+"\n";
+            break;
+        case TAC_BIGGER:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%edx\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\tcmpl\t%eax,\t%edx\n\tsetg\t%al\n\tmovb\t%al,\t_"+tac->res->get_text()+"\n";
+            break;
+        case TAC_SMALLER:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%edx\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\tcmpl\t%eax,\t%edx\n\tsetl\t%al\n\tmovb\t%al,\t_"+tac->res->get_text()+"\n";
+            break;
+        case TAC_AND:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%eax\n\ttestl\t%eax,\t%eax\n\tje\tL"+to_string(Lindex)+"\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\ttestl\t%eax,\t%eax\n\tje\tL"+to_string(Lindex+1)+"\n\tmovl\t$1,\t%eax\n\tjmp\tL"+to_string(Lindex+1)+"\nL"+to_string(Lindex)+":\n\tmovl\t$0,\t%eax\nL"+to_string(Lindex+1)+":\n\tmovb\t%al,\t_"+tac->res->get_text()+"\n";
+            Lindex += 2;
+            break;
+        case TAC_OR:
+            tac->op1->text = resolveSymbol(tac->op1);
+            tac->op2->text = resolveSymbol(tac->op2);
+            asm_code += "\tmovl\t_"+tac->op1->get_text()+",\t%eax\n\ttestl\t%eax,\t%eax\n\tjne\tL"+to_string(Lindex)+"\n\tmovl\t_"+tac->op2->get_text()+",\t%eax\n\ttestl\t%eax,\t%eax\n\tje\tL"+to_string(Lindex+1)+"\nL"+to_string(Lindex)+":\n\tmovl\t$1,\t%eax\n\tjmp\tL"+to_string(Lindex+2)+"\nL"+to_string(Lindex+1)+":\n\tmovl\t$0,\t%eax\nL"+to_string(Lindex+2)+":\n\tmovb\t%al,\t_"+tac->res->get_text()+"\n";
+            break;
         default:
             break;
         }
     }
-    // cout << asm_code << endl;
+    cout << asm_code << endl;
 }
